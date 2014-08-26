430fba21663cb6af2806538028562c0a23c2b595
lucasmreis <lucasmreis@gmail.com>
2014-08-26 13:11:13 -0300

    grunt clean e less

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,9 +1,23 @@
 module.exports = function (grunt) {
   grunt.initConfig({
-    jshint: ['js/**/*.js']
+    jshint: ['js/**/*.js'],
+    
+    clean: {
+      css: 'css/*.css'
+    },
+    
+    less: {
+      compile: {
+        files: {
+          'css/site.css' : 'css/site.less'
+        }
+      }
+    }
   });
   
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-less');
   
-  grunt.registerTask('default', ['jshint']);
+  grunt.registerTask('default', ['jshint', 'clean:css', 'less:compile']);
 };
