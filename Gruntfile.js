cc5eed683e9b475d330774e87f7d7a6af5a86670
lucasmreis <lucasmreis@gmail.com>
2014-08-28 12:33:18 -0300

    add concat to vendor.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,9 +1,12 @@
 module.exports = function (grunt) {
   grunt.initConfig({
-    jshint: ['js/**/*.js'],
+    jshint: [
+      'js/**/*.js'
+    ],
     
     clean: {
-      css: 'css/*.css'
+      css: 'css/*.css',
+      js: 'js/vendor.js'
     },
     
     less: {
@@ -12,12 +15,27 @@ module.exports = function (grunt) {
           'css/site.css' : 'css/site.less'
         }
       }
+    },
+    
+    concat: {
+      vendor: {
+        options: {
+          separator: ';'
+        },
+        src: [
+          'bower_components/angular/angular.min.js',
+          'bower_components/angular-swipe/dist/angular-swipe.min.js',
+          'bower_components/lodash/dist/lodash.min.js'
+        ],
+        dest: 'js/vendor.js'
+      }
     }
   });
   
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-concat');
   
-  grunt.registerTask('default', ['jshint', 'clean:css', 'less:compile']);
+  grunt.registerTask('default', ['clean:js', 'jshint', 'clean:css', 'less:compile', 'concat:vendor']);
 };
